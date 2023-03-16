0599e93a754aaa4548fba19de96b2621c312431a
gaborflorian <gabor.florian@blackbelt.hu>
2023-03-16 17:41:42 +0100

    Fix tabs and line ends

--- judo-tatami-expression-asm-validation/src/test/java/hu/blackbelt/judo/tatami/expression/asm/validation/ExpressionValidationOnAsmWorkTest.java
+++ judo-tatami-expression-asm-validation/src/test/java/hu/blackbelt/judo/tatami/expression/asm/validation/ExpressionValidationOnAsmWorkTest.java
@@ -9,13 +9,13 @@ package hu.blackbelt.judo.tatami.expression.asm.validation;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -52,49 +52,49 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class ExpressionValidationOnAsmWorkTest {
 
-	public static final String NORTHWIND = "northwind";
+    public static final String NORTHWIND = "northwind";
 
     TransformationContext transformationContext;
     ExpressionValidationOnAsmWork expressionValidationOnAsmWork;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		Demo demo = new Demo();
-		PsmModel psmModel = demo.fullDemo();
+    @BeforeEach
+    void setUp() throws Exception {
+        Demo demo = new Demo();
+        PsmModel psmModel = demo.fullDemo();
 
-		AsmModel asmModel = buildAsmModel().build();
-		executePsm2AsmTransformation(psm2AsmParameter()
-				.psmModel(psmModel)
-				.asmModel(asmModel));
+        AsmModel asmModel = buildAsmModel().build();
+        executePsm2AsmTransformation(psm2AsmParameter()
+                .psmModel(psmModel)
+                .asmModel(asmModel));
 
-		MeasureModel measureModel = buildMeasureModel().name(psmModel.getName()).build();
-		executePsm2MeasureTransformation(psm2MeasureParameter()
-				.psmModel(psmModel)
-				.measureModel(measureModel));
+        MeasureModel measureModel = buildMeasureModel().name(psmModel.getName()).build();
+        executePsm2MeasureTransformation(psm2MeasureParameter()
+                .psmModel(psmModel)
+                .measureModel(measureModel));
 
-		ExpressionModel expressionModel = buildExpressionModel().name(asmModel.getName()).build();
-		executeAsm2Expression(asm2ExpressionParameter()
-				.asmModel(asmModel)
-				.measureModel(measureModel)
-				.expressionModel(expressionModel));
+        ExpressionModel expressionModel = buildExpressionModel().name(asmModel.getName()).build();
+        executeAsm2Expression(asm2ExpressionParameter()
+                .asmModel(asmModel)
+                .measureModel(measureModel)
+                .expressionModel(expressionModel));
 
-		transformationContext = new TransformationContext(NORTHWIND);
-		transformationContext.put(expressionModel);
-		transformationContext.put(measureModel);
-		transformationContext.put(asmModel);
+        transformationContext = new TransformationContext(NORTHWIND);
+        transformationContext.put(expressionModel);
+        transformationContext.put(measureModel);
+        transformationContext.put(asmModel);
 
-		expressionValidationOnAsmWork = new ExpressionValidationOnAsmWork(transformationContext);
-	}
+        expressionValidationOnAsmWork = new ExpressionValidationOnAsmWork(transformationContext);
+    }
 
-	@Test
-	void testSimpleWorkflow() {
-		WorkFlow workflow = aNewSequentialFlow().execute(expressionValidationOnAsmWork).build();
+    @Test
+    void testSimpleWorkflow() {
+        WorkFlow workflow = aNewSequentialFlow().execute(expressionValidationOnAsmWork).build();
 
-		WorkFlowEngine workFlowEngine = aNewWorkFlowEngine().build();
-		WorkReport workReport = workFlowEngine.run(workflow);
+        WorkFlowEngine workFlowEngine = aNewWorkFlowEngine().build();
+        WorkReport workReport = workFlowEngine.run(workflow);
 
-		log.info("Workflow completed with status {}", workReport.getStatus(), workReport.getError());
-		assertThat(workReport.getStatus(), equalTo(WorkStatus.COMPLETED));
-	}
+        log.info("Workflow completed with status {}", workReport.getStatus(), workReport.getError());
+        assertThat(workReport.getStatus(), equalTo(WorkStatus.COMPLETED));
+    }
 
 }
--- judo-tatami-psm2asm/src/test/java/hu/blackbelt/judo/tatami/psm2asm/Psm2AsmTypeTest.java
+++ judo-tatami-psm2asm/src/test/java/hu/blackbelt/judo/tatami/psm2asm/Psm2AsmTypeTest.java
@@ -9,13 +9,13 @@ package hu.blackbelt.judo.tatami.psm2asm;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -56,13 +56,13 @@ public class Psm2AsmTypeTest {
 
     public static final String MODEL_NAME = "Test";
     public static final String TARGET_TEST_CLASSES = "target/test-classes";
-    
+
     public static final String TEST_MODEL_NAME = "Model";
     public static final String STRING = "java.lang.String";
     public static final String INTEGER = "java.lang.Integer";
     public static final String LONG = "java.lang.Long";
     public static final String BIG_DECIMAL = "java.math.BigDecimal";
-    
+
     public static final String FLOAT = "java.lang.Float";
     public static final String DOUBLE = "java.lang.Double";
     public static final String BOOLEAN = "java.lang.Boolean";
@@ -70,7 +70,7 @@ public class Psm2AsmTypeTest {
     public static final String DATE_TIME = "java.time.OffsetDateTime";
     public static final String TIME = "java.time.LocalTime";
     public static final String OBJECT = "java.lang.Object";
-    
+
     PsmModel psmModel;
     AsmModel asmModel;
     AsmUtils asmUtils;
@@ -91,7 +91,7 @@ public class Psm2AsmTypeTest {
         psmModel.savePsmModel(PsmModel.SaveArguments.psmSaveArgumentsBuilder()
                 .file(new File(TARGET_TEST_CLASSES, getClass().getName() + "-" + testName + "-psm.model"))
                 .build());
-        
+
         assertTrue(psmModel.isValid());
         try (Log bufferedLog = new BufferedSlf4jLogger(log)) {
             validatePsm(bufferedLog, psmModel, calculatePsmValidationScriptURI());
@@ -106,111 +106,111 @@ public class Psm2AsmTypeTest {
                 .file(new File(TARGET_TEST_CLASSES, getClass().getName() + "-" + testName + "-asm.model"))
                 .build());
     }
-    
+
     @Test
     void testType() throws Exception {
-    	
-    	EnumerationMember a = newEnumerationMemberBuilder().withName("a").withOrdinal(1).build();
-    	EnumerationMember b = newEnumerationMemberBuilder().withName("b").withOrdinal(2).build();
-    	EnumerationMember c = newEnumerationMemberBuilder().withName("c").withOrdinal(3).build();
-        
-    	EnumerationType enumType = newEnumerationTypeBuilder().withName("enum")
-    			.withMembers(ImmutableList.of(a,b,c)).build();
-    	StringType strType = newStringTypeBuilder().withName("string").withMaxLength(256).withRegExp(".*").build();
-    	
-    	NumericType intType = newNumericTypeBuilder().withName("int").withPrecision(6).withScale(0).build();
-    	NumericType longType = newNumericTypeBuilder().withName("long").withPrecision(16).withScale(0).build();
-    	NumericType bigDecimalIntType = newNumericTypeBuilder().withName("bigdecimalint").withPrecision(21).withScale(0).build();
-    	
-    	NumericType floatType = newNumericTypeBuilder().withName("float").withPrecision(6).withScale(3).build();
-    	NumericType doubleType = newNumericTypeBuilder().withName("bouble").withPrecision(10).withScale(3).build();
-    	NumericType bigDecimalType = newNumericTypeBuilder().withName("bigdecimal").withPrecision(17).withScale(8).build();
-    	
-    	BooleanType boolType = newBooleanTypeBuilder().withName("bool").build();
-    	
-    	//not supported yet
-    	PasswordType pwType = newPasswordTypeBuilder().withName("pw").build();
-    	//not supported yet
-    	XMLType xmlType = newXMLTypeBuilder().withName("xml").build();
 
-    	DateType dateType = newDateTypeBuilder().withName("date").build();
-    	TimestampType timeStampType = newTimestampTypeBuilder().withName("timestamp").build();
+        EnumerationMember a = newEnumerationMemberBuilder().withName("a").withOrdinal(1).build();
+        EnumerationMember b = newEnumerationMemberBuilder().withName("b").withOrdinal(2).build();
+        EnumerationMember c = newEnumerationMemberBuilder().withName("c").withOrdinal(3).build();
+
+        EnumerationType enumType = newEnumerationTypeBuilder().withName("enum")
+                .withMembers(ImmutableList.of(a,b,c)).build();
+        StringType strType = newStringTypeBuilder().withName("string").withMaxLength(256).withRegExp(".*").build();
+
+        NumericType intType = newNumericTypeBuilder().withName("int").withPrecision(6).withScale(0).build();
+        NumericType longType = newNumericTypeBuilder().withName("long").withPrecision(16).withScale(0).build();
+        NumericType bigDecimalIntType = newNumericTypeBuilder().withName("bigdecimalint").withPrecision(21).withScale(0).build();
+
+        NumericType floatType = newNumericTypeBuilder().withName("float").withPrecision(6).withScale(3).build();
+        NumericType doubleType = newNumericTypeBuilder().withName("bouble").withPrecision(10).withScale(3).build();
+        NumericType bigDecimalType = newNumericTypeBuilder().withName("bigdecimal").withPrecision(17).withScale(8).build();
+
+        BooleanType boolType = newBooleanTypeBuilder().withName("bool").build();
+
+        //not supported yet
+        PasswordType pwType = newPasswordTypeBuilder().withName("pw").build();
+        //not supported yet
+        XMLType xmlType = newXMLTypeBuilder().withName("xml").build();
+
+        DateType dateType = newDateTypeBuilder().withName("date").build();
+        TimestampType timeStampType = newTimestampTypeBuilder().withName("timestamp").build();
         TimeType timeType = newTimeTypeBuilder().withName("time").build();
 
-    	CustomType custom = newCustomTypeBuilder().withName("object").build();
-    	
-		Unit unit = newUnitBuilder().withName("u").build();
-		Measure m = newMeasureBuilder().withName("measure").withUnits(unit).build();
-		MeasuredType measuredType = newMeasuredTypeBuilder().withName("measuredType").withStoreUnit(unit)
-				.withPrecision(5).withScale(3).build();
-    	
-		Model model = newModelBuilder().withName(TEST_MODEL_NAME)
-				.withElements(ImmutableList.of(
-						enumType,strType,intType,longType,bigDecimalIntType,
-						floatType,doubleType,bigDecimalType,boolType,dateType,timeStampType,timeType,custom,
-						m,measuredType)).build();
+        CustomType custom = newCustomTypeBuilder().withName("object").build();
+
+        Unit unit = newUnitBuilder().withName("u").build();
+        Measure m = newMeasureBuilder().withName("measure").withUnits(unit).build();
+        MeasuredType measuredType = newMeasuredTypeBuilder().withName("measuredType").withStoreUnit(unit)
+                .withPrecision(5).withScale(3).build();
+
+        Model model = newModelBuilder().withName(TEST_MODEL_NAME)
+                .withElements(ImmutableList.of(
+                        enumType,strType,intType,longType,bigDecimalIntType,
+                        floatType,doubleType,bigDecimalType,boolType,dateType,timeStampType,timeType,custom,
+                        m,measuredType)).build();
 
         psmModel.addContent(model);
 
         transform("testType");
-        
+
         final EPackage asmTestModel = asmUtils.all(EPackage.class).filter(p -> p.getName().equals(TEST_MODEL_NAME)).findAny().get();
         final Optional<EEnum> asmEnum = asmUtils.all(EEnum.class).filter(e -> e.getName().equals(enumType.getName())).findAny();
         assertTrue(asmEnum.isPresent());
         assertTrue(asmEnum.get().getEPackage().equals(asmTestModel));
-        
+
         assertThat(asmEnum.get().getEEnumLiteral(1), IsNull.notNullValue());
         assertThat(asmEnum.get().getEEnumLiteral(2), IsNull.notNullValue());
         assertThat(asmEnum.get().getEEnumLiteral(3), IsNull.notNullValue());
         assertTrue(asmEnum.get().getEEnumLiteral(1).getLiteral().equals(a.getName()));
         assertTrue(asmEnum.get().getEEnumLiteral(2).getLiteral().equals(b.getName()));
         assertTrue(asmEnum.get().getEEnumLiteral(3).getLiteral().equals(c.getName()));
-        
+
         final Optional<EDataType> asmStr = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(strType.getName())).findAny();
         assertTrue(asmStr.isPresent());
         assertTrue(asmStr.get().getEPackage().equals(asmTestModel));
         assertTrue(asmStr.get().getInstanceClassName().equals(STRING));
-        
+
         final Optional<EDataType> asmInt = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(intType.getName())).findAny();
         assertTrue(asmInt.isPresent());
         assertTrue(asmInt.get().getEPackage().equals(asmTestModel));
         assertTrue(asmInt.get().getInstanceClassName().equals(INTEGER));
-        
+
         final Optional<EDataType> asmLongType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(longType.getName())).findAny();
         assertTrue(asmLongType.isPresent());
         assertTrue(asmLongType.get().getEPackage().equals(asmTestModel));
         assertTrue(asmLongType.get().getInstanceClassName().equals(LONG));
-        
+
         final Optional<EDataType> asmBigDecimalIntType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(bigDecimalIntType.getName())).findAny();
         assertTrue(asmBigDecimalIntType.isPresent());
         assertTrue(asmBigDecimalIntType.get().getEPackage().equals(asmTestModel));
         assertTrue(asmBigDecimalIntType.get().getInstanceClassName().equals(BIG_DECIMAL));
-        
+
         final Optional<EDataType> asmFloatType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(floatType.getName())).findAny();
         assertTrue(asmFloatType.isPresent());
         assertTrue(asmFloatType.get().getEPackage().equals(asmTestModel));
         assertTrue(asmFloatType.get().getInstanceClassName().equals(FLOAT));
-        
+
         final Optional<EDataType> asmDoubleType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(doubleType.getName())).findAny();
         assertTrue(asmDoubleType.isPresent());
         assertTrue(asmDoubleType.get().getEPackage().equals(asmTestModel));
         assertTrue(asmDoubleType.get().getInstanceClassName().equals(DOUBLE));
-        
+
         final Optional<EDataType> asmBigDecimalType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(bigDecimalType.getName())).findAny();
         assertTrue(asmBigDecimalType.isPresent());
         assertTrue(asmBigDecimalType.get().getEPackage().equals(asmTestModel));
         assertTrue(asmBigDecimalType.get().getInstanceClassName().equals(BIG_DECIMAL));
-        
+
         final Optional<EDataType> asmBoolType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(boolType.getName())).findAny();
         assertTrue(asmBoolType.isPresent());
         assertTrue(asmBoolType.get().getEPackage().equals(asmTestModel));
         assertTrue(asmBoolType.get().getInstanceClassName().equals(BOOLEAN));
-        
+
         final Optional<EDataType> asmDateType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(dateType.getName())).findAny();
         assertTrue(asmDateType.isPresent());
         assertTrue(asmDateType.get().getEPackage().equals(asmTestModel));
         assertTrue(asmDateType.get().getInstanceClassName().equals(LOCALE_DATE));
-        
+
         final Optional<EDataType> asmTimeStampType = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(timeStampType.getName())).findAny();
         assertTrue(asmTimeStampType.isPresent());
         assertTrue(asmTimeStampType.get().getEPackage().equals(asmTestModel));
@@ -225,7 +225,7 @@ public class Psm2AsmTypeTest {
         assertTrue(asmCustom.isPresent());
         assertTrue(asmCustom.get().getEPackage().equals(asmTestModel));
         assertTrue(asmCustom.get().getInstanceClassName().equals(OBJECT));
-        
+
         final Optional<EDataType> asmMeasured = asmUtils.all(EDataType.class).filter(e -> e.getName().equals(measuredType.getName())).findAny();
         assertTrue(asmMeasured.isPresent());
         assertTrue(asmMeasured.get().getEPackage().equals(asmTestModel));
